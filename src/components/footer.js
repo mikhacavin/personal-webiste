9c6e58b99cab0d86babf931b81774286e77b64d4
Mikha Cavin <57740762+mikhacavin@users.noreply.github.com>
2021-02-03 20:52:10 +0700

    update footer.js
    sedikit sentuhan kasih ibu

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,7 +11,7 @@ const Footer = () => (
     }}
   >
     <div className="container">
-      <p>A GatsbyJS Starter for Netlify CMS, Made with <span className="icon -love"><RiHeart2Line/></span> by <Link to="/">Stackrole.com</Link></p>
+      <p>Made with <span className="icon -love"><RiHeart2Line/></span> by <Link to="/">Mikha Cavin</Link></p>
     </div>
   </footer>
 )
